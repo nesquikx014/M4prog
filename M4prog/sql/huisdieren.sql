170b4042e859b10ce6c3e802e922baaedcf62d8d
naz <naz@example.com>
2025-05-12 15:03:05 +0200

    opdracht 1

---
+++ M4prog/sql/huisdieren.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Gegenereerd op: 12 mei 2025 om 12:44
+-- Serverversie: 11.7.2-MariaDB-ubu2404
+-- PHP-versie: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `huisdieren`
+--
+
+CREATE TABLE `huisdieren` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(150) NOT NULL,
+  `eigenaar` varchar(150) NOT NULL,
+  `typedier` varchar(80) NOT NULL,
+  `leeftijd` int(11) NOT NULL,
+  `geboortedatum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `huisdieren`
+--
+ALTER TABLE `huisdieren`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
