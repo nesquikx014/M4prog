d99b9f11dde8c503b32c8d5ade34d04484fc3c4e
Nazli Eroglu <n.eroglux014@gmail.com>
2025-05-19 15:24:50 +0200

    yessir

---
+++ M4prog/sql/04/04 where.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Gegenereerd op: 19 mei 2025 om 13:22
+-- Serverversie: 11.7.2-MariaDB-ubu2404
+-- PHP-versie: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `boeken`
+--
+
+CREATE TABLE `boeken` (
+  `id` int(11) NOT NULL,
+  `titel` varchar(100) DEFAULT NULL,
+  `auteur` varchar(100) DEFAULT NULL,
+  `publicatiejaar` year(4) DEFAULT NULL,
+  `genre` varchar(50) DEFAULT NULL,
+  `prijs` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `boeken`
+--
+
+INSERT INTO `boeken` (`id`, `titel`, `auteur`, `publicatiejaar`, `genre`, `prijs`) VALUES
+(1, 'De Hobbit', 'J.R.R. Tolkien', '1937', 'Fantasy', 12.99);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `boeken`
+--
+ALTER TABLE `boeken`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `boeken`
+--
+ALTER TABLE `boeken`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
