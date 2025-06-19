1ced41de5892021daa647dd9dc4c5c870de3d9f7
Nazli Eroglu <n.eroglux014@gmail.com>
2025-06-19 14:30:24 +0200

    yyeeeeeeeee

---
+++ M4prog/sql/07/ingredient.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Gegenereerd op: 19 jun 2025 om 12:28
+-- Serverversie: 11.7.2-MariaDB-ubu2404
+-- PHP-versie: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m4prog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `ingredient`
+--
+
+CREATE TABLE `ingredient` (
+  `id` int(11) DEFAULT NULL,
+  `naam` varchar(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `ingredient`
+--
+
+INSERT INTO `ingredient` (`id`, `naam`) VALUES
+(1, 'pindas');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
